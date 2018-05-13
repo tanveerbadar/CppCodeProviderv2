cbee5821016d7c9ed86f1a3cfa4bc68cc3786526
Tanveer Badar <tanveer06051984@hotmail.com>
2018-05-14 03:29:01 +0500

    Added conditional expression.

---
+++ CppCodeProvider/features/expressions/ternary_expression.h
@@ -0,0 +1,29 @@
+#ifndef TERNARY_EXPRESSION_HEADER
+#define TERNARY_EXPRESSION_HEADER
+
+#pragma once
+
+#include "common.h"
+#include "expression_types.h"
+#include <memory>
+
+namespace cpp::codeprovider::expressions
+{
+	class ternary_expression : public expression
+	{
+		std::unique_ptr<expression> e1, e2, e3;
+	public:
+		ternary_expression(std::unique_ptr<expression>, std::unique_ptr<expression>, std::unique_ptr<expression>);
+		ternary_expression(const ternary_expression&);
+
+		const expression& left() const;
+		const expression& right() const;
+		const expression& condition() const;
+		expression_type type() const;
+
+		std::unique_ptr<expression> clone() const override;
+		void write(std::ostream&) const override;
+	};
+}
+
+#endif // !
