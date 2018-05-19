eb2e1d8a9ff99d9963776a655456cd67267961d6
Tanveer Badar <tanveer06051984@hotmail.com>
2018-05-19 17:52:41 +0500

    Added common forward declarations.

---
+++ CppCodeProvider/forward_declarations.h
@@ -0,0 +1,27 @@
+namespace cpp::codeprovider
+{
+	namespace types
+	{
+		class type_declaration;
+	}
+
+	namespace declarations
+	{
+		class variable_declaration;
+	}
+
+	namespace templates
+	{
+		class template_parameter;
+	}
+
+	namespace statements
+	{
+		class block_statement;
+	}
+
+	namespace functions
+	{
+		class function;
+	}
+}
