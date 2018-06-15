bbf779e6fcd644abb0dc67eecd386a72c7e3ec50
Tanveer Badar <tanveer06051984@hotmail.com>
2018-06-15 20:07:22 +0500

    All expressions are assignable too.

--- CppCodeProvider/features/expressions/unary_expressions.cpp
+++ CppCodeProvider/features/expressions/unary_expressions.cpp
@@ -23,6 +23,16 @@ namespace cpp::codeprovider::expressions
 	{
 	}
 
+	unary_expression& unary_expression::operator=(const unary_expression& other)
+	{
+		if (this != &other)
+		{
+			e1 = other.e1->clone();
+			e_type = other.e_type;
+		}
+		return *this;
+	}
+
 	unique_ptr<expression> unary_expression::clone() const
 	{
 		return make_unique<unary_expression>(*this);
@@ -56,6 +66,16 @@ namespace cpp::codeprovider::expressions
 	primitive_expression::primitive_expression(const string& p)
 		:e1(p)
 	{
+	}
+
+	const string& primitive_expression::expr() const
+	{
+		return e1;
+	}
+
+	expression_type primitive_expression::type() const
+	{
+		return expression_type::primitive;
 	}
 
 	unique_ptr<expression> primitive_expression::clone() const
