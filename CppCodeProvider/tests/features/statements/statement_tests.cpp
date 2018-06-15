7b2f261f5003f6be0ef84f4f4718f87de070f3c8
Tanveer Badar <tanveer06051984@hotmail.com>
2018-06-15 22:30:24 +0500

    Added test cases for if_statement.

--- CppCodeProvider/tests/features/statements/statement_tests.cpp
+++ CppCodeProvider/tests/features/statements/statement_tests.cpp
@@ -31,6 +31,8 @@ BOOST_AUTO_TEST_CASE(block_statement_tests)
 {
 	block_statement block;
 
+	BOOST_TEST(block.statements().size() == 0);
+
 	auto copy1(block);
 
 	const auto& body1 = block.statements();
@@ -58,6 +60,8 @@ BOOST_AUTO_TEST_CASE(block_statement_tests)
 BOOST_AUTO_TEST_CASE(for_loop_tests)
 {
 	auto stmt = make_unique<for_loop>();
+
+	BOOST_TEST(stmt->statements().size() == 0);
 
 	auto copy1(*stmt);
 	const auto& body1 = stmt->statements();
@@ -106,6 +110,8 @@ BOOST_AUTO_TEST_CASE(while_loop_tests)
 {
 	auto stmt = make_unique<while_loop>(make_unique<primitive_expression>("1"));
 
+	BOOST_TEST(stmt->statements().size() == 0);
+
 	auto copy1(*stmt);
 	const auto& body1 = stmt->statements();
 	auto& body2 = stmt->statements();
@@ -140,6 +146,51 @@ BOOST_AUTO_TEST_CASE(while_loop_tests)
 	BOOST_TEST(copy2.statements().size() == 2);
 	BOOST_TEST(dynamic_cast<const primitive_expression&>(copy2.condition()).expr() == "2");
 	BOOST_TEST(copy2.style() == while_loop_style::do_while_loop);
+}
+
+BOOST_AUTO_TEST_CASE(if_statement_tests)
+{
+	auto stmt = make_unique<if_statement>(make_unique<primitive_expression>("1"));
+
+	BOOST_TEST(stmt->if_block().statements().size() == 0);
+	BOOST_TEST(dynamic_cast<const primitive_expression&>(stmt->condition()).expr() == "1");
+	BOOST_TEST(stmt->else_block().statements().size() == 0);
+
+	auto& condition = stmt->condition();
+	const auto& condition2 = stmt->condition();
+	auto copy1(*stmt);
+	const auto& body1 = stmt->if_block().statements();
+	auto& body2 = stmt->if_block().statements();
+	body2.push_back(make_unique<expression_statement>(make_unique<primitive_expression>("1")));
+	body2.emplace_back(make_unique<expression_statement>(make_unique<primitive_expression>("2")));
+
+	const auto& body3 = stmt->else_block().statements();
+	auto& body4 = stmt->else_block().statements();
+	body4.push_back(make_unique<expression_statement>(make_unique<primitive_expression>("3")));
+	body4.emplace_back(make_unique<expression_statement>(make_unique<primitive_expression>("4")));
+
+	stmt->condition(make_unique<primitive_expression>("2"));
+
+	BOOST_TEST(dynamic_cast<const primitive_expression&>(stmt->condition()).expr() == "2");
+	BOOST_TEST(stmt->if_block().statements().size() == 2);
+	BOOST_TEST(stmt->else_block().statements().size() == 2);
+
+	auto other = stmt->clone();
+
+	boost::test_tools::output_test_stream stream;
+
+	stream << *other;
+	other->write(stream);
+
+	auto copy2(*stmt);
+	BOOST_TEST(copy2.if_block().statements().size() == 2);
+	BOOST_TEST(copy2.else_block().statements().size() == 2);
+	BOOST_TEST(dynamic_cast<const primitive_expression&>(copy2.condition()).expr() == "2");
+
+//	copy2 = *stmt;
+	BOOST_TEST(copy2.if_block().statements().size() == 2);
+	BOOST_TEST(copy2.else_block().statements().size() == 2);
+	BOOST_TEST(dynamic_cast<const primitive_expression&>(copy2.condition()).expr() == "2");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
