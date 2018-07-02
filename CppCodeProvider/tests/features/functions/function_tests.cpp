c252853d377471acd98ebdcf1066f204655af1d1
Tanveer Badar <tanveer06051984@hotmail.com>
2018-07-02 21:37:52 +0500

    Updated build configuration, adding a blank test case.

---
+++ CppCodeProvider/tests/features/functions/function_tests.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE cpp_code_provider_tests
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+BOOST_AUTO_TEST_CASE(function_tests)
+{
+
+}
